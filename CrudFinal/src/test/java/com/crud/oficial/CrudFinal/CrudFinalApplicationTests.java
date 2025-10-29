7efd72e56dfdcc9d2117fd63b034276f7daadd18
CAIO THIAGO DA MATA GOMES <caio_t_gomes@estudante.sesisenai.org.br>
2025-10-29 19:12:52 -0300

    primeiro commit

---
+++ CrudFinal/src/test/java/com/crud/oficial/CrudFinal/CrudFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.oficial.CrudFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
